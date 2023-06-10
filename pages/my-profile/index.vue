8241687c4075285a3e0589391d3c6740c0dc958a
paulopariz <paulopariz01@gmail.com>
2023-06-10 00:41:13 -0300

    lógica de deletar o usuario

--- pages/my-profile/index.vue
+++ pages/my-profile/index.vue
@@ -4,7 +4,18 @@
   <div v-else>
     <section class="absolute left-1/2 -translate-x-1/2 w-full h-full">
       <div class="mt-12 flex flex-col items-center justify-center container m-auto">
-        <img :src="infoUser.photoURL" :alt="infoUser.displayName" class="w-36 h-36" />
+        <div v-if="infoUser.photoURL && user.photoURL !== 'https://github.com/.png'">
+          <img :src="infoUser.photoURL" :alt="infoUser.displayName" class="w-36 h-36" />
+        </div>
+
+        <div
+          v-else
+          class="grid h-36 w-36 place-content-center rounded-lg bg-gray-100 t dark:bg-zinc-900"
+          :title="user.displayName"
+        >
+          <h1 class="text-3xl text-gray-600 dark:text-gray-200">{{ firstLetter }}</h1>
+        </div>
+
         <h1 class="dark:text-gray-200 text-3xl">{{ infoUser.displayName }}</h1>
         <p class="dark:text-gray-200 text-xl">{{ infoUser.email }}</p>
         <span class="dark:text-gray-200 text-xl"
@@ -13,6 +24,15 @@
         <span class="dark:text-gray-200 text-xl">Ultimo login: {{ lastLoginDate }}</span>
       </div>
     </section>
+
+    <div class="fixed bottom-12 right-12 flex items-center gap-3">
+      <NuxtLink to="/my-profile/edit" class="bg-emerald-600 px-3 py-2 text-white"
+        >Editar perfil</NuxtLink
+      >
+      <button @click="deleteUser" class="bg-red-600 px-3 py-2 text-white">
+        Deletar meu perfil
+      </button>
+    </div>
   </div>
 </template>
 
@@ -36,6 +56,10 @@ export default {
 
   computed: {
     ...mapState(["loading"]),
+
+    firstLetter() {
+      return this.user.displayName.charAt(0).toLocaleUpperCase();
+    },
   },
 
   mounted() {
@@ -53,6 +77,24 @@ export default {
         this.lastLoginDate = formatDateLastLogin;
       }
     });
+  },
+
+  methods: {
+    deleteUser() {
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          user
+            .delete()
+            .then(() => {
+              this.$router.push("/");
+              console.log("usuario excluido");
+            })
+            .catch((error) => {
+              console.error("erro ao exlcuir: ", error);
+            });
+        }
+      });
+    },
   },
 };
 </script>
